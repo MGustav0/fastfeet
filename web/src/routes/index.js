d9bf4f2ce720aa7e092bbbdeee0187bf529c68a1
Gustavo Moreira <gustavomoreira88@gmail.com>
2020-03-31 18:08:31 -0500

    Rotas configuradas

---
+++ web/src/routes/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Switch } from 'react-router-dom';
+
+import Route from './Route';
+
+import SignIn from '../pages/SignIn';
+
+import Couriers from '../pages/Couriers';
+import DeliveryProblems from '../pages/DeliveryProblems';
+import Orders from '../pages/Orders';
+import Recipients from '../pages/Recipients';
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route path="/" exact component={SignIn} />
+
+      <Route path="/couriers" component={Couriers} isPrivate />
+      <Route path="/problems" component={DeliveryProblems} isPrivate />
+      <Route path="/orders" component={Orders} isPrivate />
+      <Route path="/recipients" component={Recipients} isPrivate />
+
+      <Route
+        path="/"
+        component={() => <h1>Erro 404: Esta página não existe!</h1>}
+      />
+    </Switch>
+  );
+}
