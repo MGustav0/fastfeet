8d71190c4d03d7739b5e45c7f937146a7f600c0a
Gustavo Moreira <gustavomoreira88@gmail.com>
2020-04-01 01:01:38 -0500

    Autenticação estilizada

--- web/src/pages/SignIn/index.js
+++ web/src/pages/SignIn/index.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.svg';
 
 export default function SignIn() {
-  return <h1>Login</h1>;
+  return (
+    <>
+      <Form>
+        <img src={logo} alt="FastFeet" />
+        <div>
+          SEU E-MAIL
+          <Input name="email" type="email" placeholder="exemplo@email.com" />
+        </div>
+        <div>
+          SUA SENHA
+          <Input
+            name="password"
+            type="password"
+            placeholder="***************"
+          />
+        </div>
+        <button type="submit">Entrar no sistema</button>
+      </Form>
+    </>
+  );
 }
 
 // #7D40E7
--- web/src/pages/_layouts/auth/styles.js
+++ web/src/pages/_layouts/auth/styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { darken } from 'polished';
 
 export const Wrapper = styled.div`
   height: 100%;
@@ -6,4 +7,87 @@ export const Wrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+`;
+
+export const Content = styled.div`
+  width: 360px;
+  height: 415px;
+  text-align: center;
+  background: #fff;
+  border-radius: 5px;
+
+  form {
+    display: flex;
+    flex-direction: column;
+    margin-top: 30px;
+    align-items: center;
+
+    div {
+      display: flex;
+      flex-direction: column;
+      text-align: start;
+      font-weight: bold;
+    }
+
+    input {
+      background: #fff;
+      border: 2px solid;
+      border-color: #dedede;
+      border-radius: 5px;
+      width: 300px;
+      height: 45px;
+      padding: 0 15px;
+      color: #000;
+      margin-top: 8px;
+      margin-bottom: 14px;
+      font-weight: 600;
+
+      &::placeholder {
+        color: #999999;
+        font-weight: normal;
+      }
+    }
+
+    span {
+      color: #f64c75;
+      align-self: flex-start;
+      margin: 0 0 10px;
+      font-weight: bold;
+    }
+
+    button {
+      font-size: 16px;
+      font-weight: bold;
+      margin: 10px px;
+      width: 300px;
+      height: 45px;
+      border: 0;
+      border-radius: 5px;
+      color: #fff;
+      background: #7d40e7;
+      transition: background 0.2s;
+
+      &:hover {
+        background: ${darken(0.2, '#7d40e7')};
+      }
+    }
+
+    a {
+      color: #fff;
+      margin-top: 15px;
+      font-size: 16px;
+      opacity: 0.8;
+      transition: background 0.3s;
+
+      &:hover {
+        opacity: 1;
+      }
+    }
+
+    img {
+      width: 75%;
+      align-items: center;
+      margin: 30px 0 40px;
+    }
+  }
 `;
