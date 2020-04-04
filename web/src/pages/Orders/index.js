15ec82fa921d971446244b7323119fa465141b43
Gustavo Moreira <gustavomoreira88@gmail.com>
2020-04-03 21:19:20 -0500

    Layout geral ajustado

--- web/src/pages/Orders/index.js
+++ web/src/pages/Orders/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import api from '~/services/api';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function Orders() {
   api.get('delivery');
-  return <h1>Encomendas</h1>;
+  return (
+    <Container>
+      <h1>Encomendas</h1>
+    </Container>
+  );
 }
--- web/src/pages/_layouts/default/styles.js
+++ web/src/pages/_layouts/default/styles.js
@@ -2,5 +2,15 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.div`
   height: 100%;
+  background: #fff;
+`;
+
+export const Content = styled.div`
+  height: 100%;
   background: #f5f5f5;
+  max-width: 1440px;
+  height: 740px;
+  /* Alterar de "auto" para "740px" em caso de erro */
+  min-height: auto;
+  margin: 0 auto;
 `;
